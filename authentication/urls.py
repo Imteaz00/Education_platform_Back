9507d080a6ba9611088b769497126f9ac38c58e4
Imteaz00 <imteaz969@gmail.com>
2023-10-27 23:46:30 +0600

    After auth

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path("signup", views.signup, name = "signup"),
     path("signin", views.signin, name = "signin"),
     path("signout", views.signout, name = "signout"),
+    path("admin_pass", views.admin_pass, name = "admin_pass"),
+    path("create_admin", views.create_admin, name = "create_admin"),
+    path("t_signup", views.teacher_signup, name = "t_signup"),
+    # path("profile", views.profile, name = "profile"),
+    path("edit_profile", views.edit_profile, name = "edit_profile"),
+    path("test", views.test, name = "test"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,42 +1,99 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from .serializers import UserinfoSerializer
+from django.http import HttpResponse, HttpRequest, JsonResponse
+
 
 # Create your views here.
+@api_view(["GET", "POST"])
 def home(request):
-    return render(request, "index.html")
+    if request.method == "POST":
+        if "admin_signup" in request.data and request.data["admin_signin"] == 'True':
+            if request.user.is_staff:
+                return redirect("/admin")
+            else:
+                return redirect("admin_pass")
+            
+        if "signup" in request.data and request.data["signup"] == 'True':
+            return redirect("signup")
+        
+        if "t_signup" in request.data and request.data["t_signup"] == 'True':
+            return redirect("t_signup")
 
+        if "signin" in request.data and request.data["signin"] == 'True':
+            return redirect("signin")
+        
+        if "signout" in request.data and request.data["signout"] == 'True':
+            return redirect("signout")
+
+    users = User.objects.all()
+    serializer = UserinfoSerializer(users, many = True)
+    return Response(serializer.data)
+
+@api_view(["GET","POST"])
 def signout(request):
     logout(request)
     messages.success(request, 'Logged out')
     return redirect("home")
 
 
+@api_view(["POST"])
 def signin(request):
-
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
+        username = request.data["username"]
+        password = request.data["password"]
 
         userinfo = authenticate(username=username, password= password)
 
         if userinfo is not None:
             login(request, userinfo)
-            return render(request, "index.html", {"username": username})
+            return redirect("home")
 
         else:
             messages.error(request, "Error in Username or Password")
+    return Response("hsuijdhuf")
 
-    return render(request, "signin.html")
 
+@api_view(["POST"])
+def teacher_signup(request):
+     if request.method == "POST":
+        username = request.data["username"]
+        # email = request.data["email"]
+        password = request.data["password"]
+        password2 = request.data["password2"]
+
+        if User.objects.filter(username=username):
+            print(123)
+            messages.error(request, "Username exists already")
+            return redirect("t_signup")
+        
+        if password!= password2:
+            print(234)
+            messages.error(request, "Both password does not match")
+            return redirect("t_signup")
+        
+        userinfo = User.objects.create_user(username, password)
+
+        userinfo.save()
+        
+
+        messages.success(request, "Account Created Successfully!")
+
+        return redirect("home")
+
+
+@api_view(["POST"])
 def signup(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
-        password = request.POST["password"]
-        password2 = request.POST["password2"]
+        username = request.data["username"]
+        email = request.data["email"]
+        password = request.data["password"]
+        password2 = request.data["password2"]
+
 
         if User.objects.filter(username=username):
             messages.error(request, "Username exists already")
@@ -51,12 +108,61 @@ def signup(request):
 
         messages.success(request, "Account Created Successfully!")
 
-        return redirect("signin")
+        return redirect("home")
 
-    return render(request, "signup.html")
 
-def edit_profile(request):
+@api_view(["POST"])
+def create_admin(request):
     if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
+        username = request.data["username"]
+        # email = request.data["email"]
+        password = request.data["password"]
+        password2 = request.data["password2"]
 
+
+        if User.objects.filter(username=username):
+            messages.error(request, "Username exists already")
+            return redirect("create_admin")
+        
+        if password!= password2:
+            messages.error(request, "Both password does not match")
+            return redirect("create_admin")
+        
+        userinfo = User.objects.create_superuser(username, password)
+
+        userinfo.save()
+        
+        userinfo = authenticate(username=username, password=password)
+        login(request, userinfo)
+        messages.success(request, "Account Created Successfully!")
+        return redirect("home")
+    
+
+@api_view(["PUT"])
+def edit_profile(request):
+
+    if request.method == "PUT":
+        abc = request.data["abc"]
+        request.user.abc = abc
+        return redirect("edit_profile")
+    return Response("jaidfh")
+
+
+@api_view(["POST"])
+def admin_pass(request):
+    if request.method == "POST":
+        superpass = request.data["superpass"]
+        if superpass == "Hehheboy":
+            request.user.is_staff = True
+            return redirect("create_admin")
+        
+@api_view(["POST","GET"])
+def test(request):
+    print(request.user.is_staff)
+    if request.method == "PUT":
+        abc = request.data["abc"]
+        request.user.abc = abc
+        return redirect("edit_profile")
+    return Response("jaidfh")
+
+        
